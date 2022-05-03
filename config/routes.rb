3c4a52cfd62bbf42393301854482ed6068f5d201
Derek Mason <3derekmason@gmail.com>
2022-05-03 14:42:32 -0600

    add about route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'home/about'
   # Defines the root path route ("/")
   root 'home#index'
   # root "articles#index"
+
 end
