5db6be1cfe0dae5773689bb8f6dbc84a2957a456
Derek Mason <3derekmason@gmail.com>
2022-05-05 09:39:34 -0600

    change add contact notice

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -25,7 +25,7 @@ class ContactsController < ApplicationController
 
     respond_to do |format|
       if @contact.save
-        format.html { redirect_to contact_url(@contact), notice: "Contact was successfully created." }
+        format.html { redirect_to contact_url(@contact), notice: "Contact was added successfully." }
         format.json { render :show, status: :created, location: @contact }
       else
         format.html { render :new, status: :unprocessable_entity }
